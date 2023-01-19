551393fb74d3950c90333156eccc3e7dc360f08f
ita-jp <tn00gm@gmail.com>
2023-01-19 23:59:16 +0900

    spring initializr

---
+++ src/main/java/com/example/springauth0/Springauth0Application.java
@@ -0,0 +1,13 @@
+package com.example.springauth0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springauth0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springauth0Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springauth0/Springauth0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springauth0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springauth0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
